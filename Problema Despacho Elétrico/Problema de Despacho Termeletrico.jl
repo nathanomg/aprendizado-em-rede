a065d9f9801fb39a47385b2d102d4059697f7fe6
nathanomg <nathan.santos@omegaenergia.com.br>
2023-04-06 15:26:13 -0300

    Update Problema de Despacho Termeletrico.jl

--- Problema Despacho Elétrico/Problema de Despacho Termeletrico.jl
+++ Problema Despacho Elétrico/Problema de Despacho Termeletrico.jl
@@ -38,17 +38,25 @@ model = JuMP.Model(GLPK.Optimizer)
 @variable(model, flow_se_s[1:T, 1:S]          >= 0)
 @variable(model, flow_s_se[1:T, 1:S]          >= 0)
 
+#ToDo: custo da reserva
 @objective(model, Min, sum(((1+disc_rate)^(t) * sum(cost .* thermo_gen[t, :, :]) + sum(deficit[t,:, :])*deficit_cost ) for t in 1:T))
 
 @constraint(model, balanco_energia_se[t = 1:T, s=1:S]           , deficit[t,1, s] + flow_s_se[t, s]                                      .== demand[1][t] + flow_se_s[t, s]) 
-@constraint(model,                   [t = 1:T, s=1:S]           , gen_tot[t, s] + deficit[t,2, s] + flow_se_s[t, s]                      .== demand[2][t] + flow_s_se[t, s]) 
+@constraint(model,                   [t = 1:T, s=1:S]           , sum(thermo_gen[t,:, s]) + sum(wind_gen[t,:, s]) + sum(hidr_gen[t,:, s]) + deficit[t,2, s] + flow_se_s[t, s] .== demand[2][t] + flow_s_se[t, s]) 
 @constraint(model,        max_flow_se[t = 1:T, s=1:S]           , flow_se_s[t, s]                                                         <= F) 
-@constraint(model,         max_flow_s[t = 1:T, s=1:S]           , flow_s_se[t, s]                                                         <= F) 
-@constraint(model,       max_variacao[t = 2:T, s=1:S]           , thermo_gen[t,3, s] - thermo_gen[t-1,3, s]                               <= 1.)
-@constraint(model,         generation[t = 1:T, s=1:S]           , sum(thermo_gen[t,:, s]) + sum(wind_gen[t,:, s]) + sum(hidr_gen[t,:, s]) == gen_tot[t, s])
-@constraint(model,         limite_cap[t = 1:T, i = 1:nT, s=1:S] , thermo_gen[t,i, s]                                                      <= capacity[i][t]) 
+@constraint(model,         max_flow_s[t = 1:T, s=1:S]           , flow_s_se[t, s]                                                         <= F)
+@constraint(model,         limite_cap[t = 1:T, i = 1:nT, s=1:S] , thermo_gen[t,i, s]  + thermo_res[t,i, s]                                <= capacity[i][t]) 
 @constraint(model,         wind_const[t = 1:T, i = 1:nW, s=1:S] , wind_gen[t,i, s] + wind_curtail[t,i, s]                                 == wind_scen[t, s])
 @constraint(model,         hidr_const[t = 1:T, i = 1:nH, s=1:S] , hidr_gen[t,i, s] + hidr_curtail[t,i, s]                                 == hidr_scen[t]*produtib[i])
+@constraint(model,        max_flow_se[t = 1:T, s=1:S]           , sum(thermo_res[t,:,s]) >= sum(demand[2][t])*0.05 )
+@constraint(model,        max_flow_se[t = 1:T+1, s=1:S]         , v[]  =  v + ena - geracao - vertimento)
+
+#ToDo: modelagem reservatorio
+#ToDo: flow[t, s, de, pa] 
+#media reservatorios no tempo. v ini = max; vol <> infinito
+#periodo de 60 meses e resultados em 1, 2, 5, 10
+
+
 
 optimize!(model)
 solution_summary(model)
